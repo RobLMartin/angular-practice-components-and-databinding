a94b858df269655b83b6eb2840aa3c578a0e2706
Rob Martin <Robert@sawsoftmill.com>
2017-10-13 17:53:31 -0400

    event emitter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // pre-populated server array.
   serverElements = [
     {type: 'server', name: 'testserver', content: 'just a test'},
     {type: 'server', name: 'Another Server', content: 'more content'},
     {type: 'blueprint', name: 'A Blueprint', content: 'content'}
 ];
+
+// push data to our server array.
+onServerAdded(serverData: {serverName: string, serverContent: string}) {
+  this.serverElements.push({
+    type: 'server',
+    name: serverData.serverName,
+    content: serverData.serverContent
+  });
+}
+
+// push data to our server array.
+onBlueprintAdded(blueprintData: {serverName: string, serverContent: string}) {
+  this.serverElements.push({
+    type: 'blueprint',
+    name: blueprintData.serverName,
+    content: blueprintData.serverContent
+  });
+}
 
 
 }
